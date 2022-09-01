78ffac25af83da8f0bd37722c992e4e7cba96cc3
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-08-31 21:23:24 -0400

    Setup home endpoint get method

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Home path");
+})
+
 app.listen(8080, () => {
     console.log("Server listening on port 8080");
 });
