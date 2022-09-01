59d5c21521ff74725b4f53013856e5ebcc693bed
Hyunchol Jun <hyunchol.jun@gmail.com>
2022-08-31 21:37:02 -0400

    Write json data and define all endpoints

--- index.js
+++ index.js
@@ -5,9 +5,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Home path");
-})
+app.get("/user", (req, res) => {
+    res.send("User path");
+});
+
+app.put("/buy-house", (req, res) => {
+    res.send("Buy house path");
+
+});
+
+app.put("/sell/:id", (req, res) => {
+    res.send("sell path");
+
+});
+
+app.post("/buy", (req, res) => {
+    res.send("Buy path");
+
+});
+
+app.put("/water", (req, res) => {
+    res.send("water path");
+
+});
+
+app.put("/fertilize", (req, res) => {
+    res.send("fertilize path");
+
+});
+
+app.put("/sleep", (req, res) => {
+    res.send("sleep path");
+
+});
+
+
 
 app.listen(8080, () => {
     console.log("Server listening on port 8080");
