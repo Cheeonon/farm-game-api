c3e6069a02a2e92618657425db518f54660ea2cd
채언 <Cheeon@Junghoonui-MacBookPro.local>
2023-01-14 10:59:04 -0500

    Add PORT env variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const marketController = require("./controllers/marketController");
 const userController = require("./controllers/userController");
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
